8a0f83f67261ee5ed9463f99fe51bafb50bad95d
tanwei <tanwei.rush@gmail.com>
2025-08-18 11:54:36 +0800

    1. Route 增加自己的 timeout 2.

--- v3apisix/route.go
+++ v3apisix/route.go
@@ -11,6 +11,7 @@ const (
 	RouteEnableFalse = RouteEnable(0)
 )
 
+// https://apisix.apache.org/zh/docs/apisix/3.10/admin-api/#route-request-body-parameters
 type Route struct {
 	ID      string         `json:"id"`                // 路由 ID。必备字段，Create/Update/Delete 时需要指定。
 	Name    string         `json:"name,omitempty"`    // 路由名称。
@@ -19,14 +20,15 @@ type Route struct {
 	Plugins map[string]any `json:"plugins,omitempty"` // 插件列表，可以配置多个插件。
 	// Script 与 Plugin 不兼容，并且 Script 优先执行 Script，这意味着配置 Script 后，Route 上配置的 Plugin 将不被执行。
 	// 理论上，在 Script 中可以编写任意 Lua 代码，你也可以直接调用已有的插件以复用已有的代码。
-	Script      string      `json:"script,omitempty"`
-	ServiceId   string      `json:"service_id,required"`    // 需要绑定的 Service id
-	Host        []string    `json:"hosts,omitempty"`        // 匹配规则，域名，比如 foo.com；也支持泛域名，比如 *.foo.com。
-	RemoteAddrs []string    `json:"remote_addrs,omitempty"` // 匹配规则，表示允许有多个不同 IP 地址，符合其中任意一个即可。
-	Methods     []string    `json:"methods,omitempty"`      // 匹配规则，请求方法，比如 GET、POST 等。
-	Priority    int         `json:"priority,omitempty"`     // 匹配规则，优先级，值越小优先级越高。
-	Vars        vars.Vars   `json:"vars,omitempty"`         // 匹配规则，支持通过请求头，请求参数、Cookie 进行路由匹配。
-	Status      RouteEnable `json:"status"`                 // 路由状态，1 表示启用，0 表示禁用。
+	Script      string           `json:"script,omitempty"`
+	ServiceId   string           `json:"service_id,required"`    // 需要绑定的 Service id
+	Host        []string         `json:"hosts,omitempty"`        // 匹配规则，域名，比如 foo.com；也支持泛域名，比如 *.foo.com。
+	RemoteAddrs []string         `json:"remote_addrs,omitempty"` // 匹配规则，表示允许有多个不同 IP 地址，符合其中任意一个即可。
+	Methods     []string         `json:"methods,omitempty"`      // 匹配规则，请求方法，比如 GET、POST 等。
+	Timeout     *UpstreamTimeout `json:"timeout,omitempty"`      // 为 Route 设置 Upstream 连接、发送消息和接收消息的超时时间（单位为秒）。该配置将会覆盖在 Upstream 中配置的 timeout 选项。
+	Priority    int              `json:"priority,omitempty"`     // 匹配规则，优先级，值越小优先级越高。
+	Vars        vars.Vars        `json:"vars,omitempty"`         // 匹配规则，支持通过请求头，请求参数、Cookie 进行路由匹配。
+	Status      RouteEnable      `json:"status"`                 // 路由状态，1 表示启用，0 表示禁用。
 }
 
 func (r Route) SourcePath() string {
